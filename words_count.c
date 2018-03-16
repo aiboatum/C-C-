6da8abe05f707672575bfe8cc3669aec53bccd2b
aiboatum <x_euler@163.com>
2018-03-16 15:00:07 +0800

    commit

--- words_count.c
+++ words_count.c
@@ -1,20 +1,27 @@
 #include <stdio.h>//利用fscanf和fprintf函数完成此功能
-//fscanf和fprintf sscanf 和 sprintf 更加灵活
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 typedef struct Words{
         char s[30];
         int count;
-        int tag;
+        int tag;//标志该结构体是否已被使用
 }Words;
-// int f_scanf(FILE *,char *);
-// int f_scanf(FILE *p,char *ch){
-//     fscanf(p,"%s",ch);
-//     char *t=ch;
-//     while(*t){
-//             if()
-//     }    
-// }
+int f_scanf(FILE *,char *);
+int f_scanf(FILE *p,char *ch){
+        int c;
+        while(!isalpha(c=getc(p))&&c!=EOF)
+        continue;//排除开头非字母
+        ch[0]=c;
+        //for(int i=0;fscanf(p,"%c",c)&&isalpha(c);i++)
+        //for(int i=0;(c=getc(p))&&isalpha(c);i++)
+        int i=1;
+        while(isalpha(c=getc(p))&&c!=EOF){
+        ch[i++]=c;
+        }
+        ch[i]='\0';
+        return 1;
+}
 int main(void){
         struct Words s[1000]={{.count=0,.tag=0}};
         FILE *p;
@@ -23,10 +30,8 @@ int main(void){
                 puts("Error occurs!!");
                 exit(1);
         }
-       
-        //int i=0;
         char temp[30];       
-        while((fscanf(p,"%s",temp)==1)){
+        while(f_scanf(p,temp)==1){
                    for(int i=0;i<1000;++i){
                         if(s[i].tag==0){
                         strcpy(s[i].s,temp);  
@@ -34,12 +39,11 @@ int main(void){
                         s[i].count++;
                         break;
                         }
-                        else if(strcmp(temp,s[i].s)==0)
-                        {s[i].count++;break;}
-                   
-                //fprintf(stdout,"%s\t",s[i].s);
+                        else if(strcmp(temp,s[i].s)==0){
+                        s[i].count++;
+                        break;
+                        }
                 }
-                
         }//从中file中读取一个单词放入s结构体的char s[30]中 
         fclose(p);
         for(int i=0;s[i].tag;i++)

1e1b5c44e6e5d906e322f97c21f895d409ae153a
aiboatum <37316240+aiboatum@users.noreply.github.com>
2019-03-13 15:19:37 +0800

    Update
    Simplify the code.

--- Ispalindrome.c
+++ Ispalindrome.c
@@ -3,19 +3,12 @@
 #include <string.h>
 int  foo(char *);
 int  foo(char *s){
-    int n=strlen(s)-1;
-    char *p1=s,*p2=s+n;
-    while(p1<=p2){
-        if(*p1!=*p2)return 0;
-        p1++;
-        p2--;
-    }
+    for(int i=0,j=strlen(s)-1;i<j;)if(s[i++]!=s[j--])return 0;
     return 1;
 }
 
 int main(void) {
     char ch[]="aaa000000000000000aaa";
-
     printf("%d",foo(ch));
 
 }

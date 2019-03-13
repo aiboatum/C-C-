6883068e41fbfc94ca535297b8ce3fd2b149a551
aiboatum <1329916393@qq.com>
2019-03-13 19:55:33 +0800

    “Update”

--- Ispalindrome.c
+++ Ispalindrome.c
@@ -6,7 +6,6 @@ int  foo(char *s){
     for(int i=0,j=strlen(s)-1;i<j;)if(s[i++]!=s[j--])return 0;
     return 1;
 }
-
 int main(void) {
     char ch[]="aaa000000000000000aaa";
     printf("%d",foo(ch));

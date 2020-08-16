512f878da1f231db2c55f7c7b396cfbaf79d113e
aiboatum <x.eulerrr@gmail.com>
2020-08-16 19:40:50 +0800

    update gitignore

--- tmp.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cstring>
-using namespace std ;
-int main(){
-    int a[101];
-    memset(a,258, sizeof(a));
-    for(int i=1;i<=10;i++)cout<<a[i]<<" ";
-    return 0;
-}
